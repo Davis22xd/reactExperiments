43c0eef5269e7de612c50de705e158f1946d87f3
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-03-28 16:40:38 -0500

    filling example information of the react table

--- my-app/src/components/table-component/table-component.js
+++ my-app/src/components/table-component/table-component.js
@@ -16,16 +16,28 @@ class TableComponent extends Component {
             </thead>
             <tbody>
             <tr>
-                <td></td>
+                <td>Pool 1</td>
+                <td>45</td>
+                <td>7</td>
+                <td>10</td>
             </tr>
             <tr>
-                <td></td>
+                <td>Pool 2</td>
+                <td>50</td>
+                <td>8</td>
+                <td>12</td>
             </tr>
             <tr>
-                <td></td>
+                <td>Pool 3</td>
+                <td>60</td>
+                <td>9</td>
+                <td>8</td>
             </tr>
             <tr>
-                <td></td>
+                <td>Pool 4</td>
+                <td>45</td>
+                <td>10</td>
+                <td>10</td>
             </tr>
             </tbody>
         </table>
