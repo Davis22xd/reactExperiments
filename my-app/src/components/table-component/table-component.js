a0c0addfbde0cae2f32f54d3b8bb1ffc795a0ca8
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-03-28 17:28:19 -0500

    filling table from data in file

--- my-app/src/components/table-component/table-component.js
+++ my-app/src/components/table-component/table-component.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './table.css'
 import DataRow from './../data-row/DataRow';
-
-class TableComponent extends Component {
-
-    render() {
-      return (
+import data from './../../data/data';
+  export default () => {
+       const rows = data.map( poolInfo => {
+           return (
+           <DataRow
+           poolName={poolInfo.poolName}
+           oxygen={poolInfo.oxygen}
+           ph={poolInfo.ph}
+           temperature={poolInfo.temperature}
+           />
+           )
+         })
+    return (
         <table>
             <thead>
             <tr>
@@ -16,34 +24,8 @@ class TableComponent extends Component {
             </tr>
             </thead>
             <tbody>
-            <DataRow
-            poolName='Pool 1'
-            oxygen='45'
-            ph='7.8'
-            temperature='12'
-            />
-            <DataRow
-            poolName='Pool 2'
-            oxygen='40'
-            ph='8'
-            temperature='12'
-            />
-            <DataRow
-            poolName='Pool 3'
-            oxygen='39'
-            ph='10'
-            temperature='12'
-            />
-            <DataRow
-            poolName='Pool 4'
-            oxygen='50'
-            ph='7.8'
-            temperature='9'
-            />
+            {rows}
             </tbody>
         </table>
       );
-    }
-  }
-  
-  export default TableComponent
+}
---
+++ my-app/src/data/data.js
@@ -0,0 +1,34 @@
+/* 
+  data source
+*/
+
+export default [
+    {
+        id:1,
+        poolName:'Pool 1',
+        oxygen:45,
+        ph:10,
+        temperature:12
+    },
+    {
+        id:2,
+        poolName:'Pool 2',
+        oxygen:55,
+        ph:11,
+        temperature:11
+    },
+    {
+        id:3,
+        poolName:'Pool 3',
+        oxygen:60,
+        ph:9,
+        temperature:12
+    },
+    {
+        id:4,
+        poolName:'Pool 4',
+        oxygen:44,
+        ph:7.8,
+        temperature:10
+    }
+]
