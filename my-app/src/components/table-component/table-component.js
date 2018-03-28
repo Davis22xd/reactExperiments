31043a439ec1b8a20e76fa06ae8175401d91079e
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-03-28 16:33:56 -0500

    layed out table component

---
+++ my-app/src/components/table-component/table-component.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './table.css'
+
+class TableComponent extends Component {
+
+    render() {
+      return (
+        <table>
+            <thead>
+            <tr>
+                <th>Pool</th>
+                <th>Ox</th>
+                <th>Ph</th>
+                <th>Temp</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+      );
+    }
+  }
+  
+  export default TableComponent
