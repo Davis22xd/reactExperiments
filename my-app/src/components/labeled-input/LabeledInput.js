975f5a6593e18aa3d0b477f5e12f1848bba959ac
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-03-29 17:24:10 -0500

    changed color font on login form container

--- my-app/src/components/labeled-input/LabeledInput.js
+++ my-app/src/components/labeled-input/LabeledInput.js
@@ -1,31 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './labeledInput.css';
 
-class LabeledInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
+const LabeledInput = (props) => ( 
+    <div>
+                <label className="inputLabel">{props.title}:</label>
+                <input className="inputElement" type={props.inputType}
+                    value={props.value}
+                    required={props.required} 
+                    onChange={props.controlFunc}/>
+            </div> 
+  );
 
-    handleChange(event) {
-        this.setState({value: this.state.value})
-    }
-
-    render() {
-        return (
-            <div>
-                <label className="inputLabel">{this.props.title}:</label>
-                <input className="inputElement" type={this.props.type}
-                    value={this.state.value}
-                    required={this.props.required} 
-                    onChange={this.handleChange}/>
-            </div>
-
-        );
-    }
-}
-
-export default LabeledInput;
+  LabeledInput.propTypes = { 
+    inputType: PropTypes.oneOf(['text', 'number','password']).isRequired,
+    title: PropTypes.string.isRequired,
+    controlFunc: PropTypes.func.isRequired,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.string
+    ]).isRequired,
+  };
+  
+  export default LabeledInput;  
--- my-app/src/components/login-form/LoginForm.js
+++ my-app/src/components/login-form/LoginForm.js
@@ -10,11 +10,22 @@ class LoginForm extends React.Component {
             name: '',
             password: ''
         };
+        
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
+  
+    handleNameChange(e) {
+		this.setState({ name: e.target.value });
+	}
+  
+    handlePasswordChange(e) {
+		this.setState({ password: e.target.value });
+	}
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name was submitted: ' + this.state.name);
         alert('A value was submitted: ' + this.state.password);
         event.preventDefault();
     }
@@ -23,8 +34,8 @@ class LoginForm extends React.Component {
         return (
             <form id="loginForm" onSubmit={this.handleSubmit}>
                 <h2 id="loginHeader">Login</h2>
-                <LabeledInput title='Nombre' type='text'  required></LabeledInput>
-                <LabeledInput title='Contraseña' type='password' required></LabeledInput>
+                <LabeledInput title={'Nombre'} inputType={'text'} value={this.state.name}  controlFunc={this.handleNameChange} required></LabeledInput>
+                <LabeledInput title={'Contraseña'} inputType={'password'} value={this.state.password} controlFunc={this.handlePasswordChange} required></LabeledInput>
                 <div className="buttonHolder">
                     <input id="logInSubmit" type="submit" value="Log In" />
                 </div>
