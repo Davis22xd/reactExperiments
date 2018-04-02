82a40aed736751e74f93046d58a7feb437daba25
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-04-02 10:37:39 -0500

    added normalizer to rest css

--- my-app/src/components/input-labeler/InputLabeler.js
+++ my-app/src/components/input-labeler/InputLabeler.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import './InputLabeler.css';
 
 const InputLabeler = (props) => (
-  <div className="inputContainer">
-    <label className="inputLabel">{props.title}:</label>
-    {props.children}
-  </div>
+    <div className="inputContainer">
+        <label className="inputLabel">{props.title}:</label>
+        {props.children}
+    </div>
 );
 
 InputLabeler.propTypes = {
-  title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired
 };
 
 export default InputLabeler;  
