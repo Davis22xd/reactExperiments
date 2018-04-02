435d5e6cecc8f2a0a7fd0f351752497eecc3bf06
davidp <davidp@administrators-MacBook-Pro-6.local>
2018-04-02 12:42:11 -0500

    adde routing to show different pages in project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import { Switch, Route } from 'react-router-dom';
+import LoginContainer from './components/login-container/LoginContainer';
+import DataForTable from './components/data-for-table/DataForTable';
 import './App.css';
-import LoginContainer from './components/login-container/LoginContainer'
 
 class App extends Component {
   render() {
     return (
-        <LoginContainer />    
+      <Switch>
+        <Route exact path='/' component={LoginContainer} />
+        <Route path='/monitoring' component={DataForTable} />
+      </Switch>
     );
   }
 }
